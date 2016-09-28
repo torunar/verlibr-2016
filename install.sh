5405d73b65933a5aeae6edebcb71c8744d57fa76
Mike Schekotov <mschekotov@cs-cart.com>
2016-09-28 23:37:58 +0400

    Script installing all dependencies added.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+apt-get install                                  \
+    texlive-latex-base texlive-latex-recommended \
+    texlive-humanities texlive-latex-extra       \
+    texlive-lang-cyrillic scalable-cyrfonts-tex  \
+    -- yes

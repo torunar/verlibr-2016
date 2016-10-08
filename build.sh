f02ce24e46263aae1e43ca263af3980958cf1b87
Mike Schekotov <mschekotov@cs-cart.com>
2016-10-08 22:18:29 +0400

    Build and install scripts added.

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env sh
+
+# build main file
 cd ./src
-pdflatex                      \
-	--file-line-error         \
-	--output-directory ../bin \
-	--halt-on-error           \
-	main.latex
+for i in 1 2 3
+do
+	pdflatex                      \
+		--file-line-error         \
+		--output-directory ../bin \
+		--halt-on-error           \
+		main.latex
+done
+
+# unite pdf files
+cd ../bin
+pdfunite               \
+	title-page.pdf     \
+	title-page-rev.pdf \
+	main.pdf           \
+	colophon.pdf       \
+	verlibr-2016.pdf
+
--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ apt-get install                                  \
     texlive-latex-base texlive-latex-recommended \
     texlive-humanities texlive-latex-extra       \
     texlive-lang-cyrillic scalable-cyrfonts-tex  \
+	poppler-utils                                \
     -- yes
